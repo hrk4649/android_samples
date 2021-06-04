216648186cfe361fe71f033870ac86659610eaac
Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
2021-06-04 14:01:11 +0900

    #4 ServiceSample initial add

---
+++ ServiceSample/app/src/main/java/okinawa/flat_e/service_sample/MainActivity.kt
@@ -0,0 +1,77 @@
+package okinawa.flat_e.service_sample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import okinawa.flat_e.service_sample.service.SampleService
+import okinawa.flat_e.service_sample.service.SampleServiceConnection
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private var serviceConnection:SampleServiceConnection? = SampleServiceConnection()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity.onCreate", "called")
+
+        startSampleService()
+
+        val textView1 = findViewById<TextView>(R.id.textView1)
+        lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                while(true) {
+                    delay(5000)
+                    val location = serviceConnection?.mService?.location
+                    location?.let {
+                        val date = Date(location.time)
+                        val txt = "$date (${location.latitude}, ${location.longitude})"
+                        runOnUiThread {
+                            textView1.text = txt
+                        }
+                    }
+                }
+            } catch (e:Exception) {
+                Log.e("MainActivity.onCreate", "error:", e)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity.onDestroy", "called")
+        stopSampleService()
+
+        serviceConnection = null
+    }
+
+    private fun startSampleService() {
+        Log.d("MainActivity.startSampleService", "start Service")
+        val serviceIntent = Intent(this.application, SampleService::class.java)
+        startForegroundService(serviceIntent)
+
+        val bindIntent = Intent(this, SampleService::class.java)
+        serviceConnection?.let {serviceConnection ->
+            Log.d("MainActivity.startSampleService", "bind Service")
+            bindService(bindIntent, serviceConnection, BIND_AUTO_CREATE)
+        }
+    }
+
+    private fun stopSampleService() {
+        if (serviceConnection?.mBound == true) {
+            Log.d("MainActivity.stopSampleService", "unbind Service")
+            unbindService(serviceConnection!!)
+        }
+
+        val serviceIntent = Intent(this.application, SampleService::class.java)
+        Log.d("MainActivity.stopSampleService", "stop service")
+        stopService(serviceIntent)
+    }
+}
---
+++ ServiceSample/app/src/main/java/okinawa/flat_e/service_sample/service/SampleService.kt
@@ -0,0 +1,129 @@
+package okinawa.flat_e.service_sample.service
+
+import android.annotation.SuppressLint
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.os.Binder
+import android.os.IBinder
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import okinawa.flat_e.service_sample.MainActivity
+import okinawa.flat_e.service_sample.R
+
+class SampleService : Service() {
+    companion object {
+        private const val ONGOING_NOTIFICATION_ID = 1
+        // https://developers.google.com/android/reference/com/google/android/gms/location/LocationRequest
+        private const val LOCATION_REQUEST_PRIORITY = LocationRequest.PRIORITY_HIGH_ACCURACY
+        private const val LOCATION_REQUEST_INTERVAL_MILLISECOND = 5000L
+    }
+
+    private val binder = SampleServiceBinder()
+
+    private var fusedLocationClient:FusedLocationProviderClient? = null
+
+    var location: Location? = null
+        private set
+
+    private val locationCallback = object: LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            location = locationResult?.lastLocation
+            Log.d("SampleService.onLocationResult","location: $location")
+        }
+    }
+
+    inner class SampleServiceBinder: Binder() {
+        fun get() = this@SampleService
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return binder
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("SampleService.onStartCommand","called")
+        prepareForeground()
+        startLocationClient()
+
+        return START_NOT_STICKY
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("SampleService.onCreate","called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopLocationClient()
+
+        Log.d("SampleService.onDestroy","called")
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationClient() {
+        val locationRequest = LocationRequest.create()
+            .setPriority(LOCATION_REQUEST_PRIORITY)
+            .setInterval(LOCATION_REQUEST_INTERVAL_MILLISECOND)
+        fusedLocationClient = FusedLocationProviderClient(this)
+        val result = fusedLocationClient?.requestLocationUpdates(
+            locationRequest, locationCallback, Looper.myLooper())
+    }
+
+    private fun stopLocationClient() {
+        fusedLocationClient?.removeLocationUpdates(locationCallback)
+        fusedLocationClient = null
+    }
+
+    private fun prepareForeground() {
+        Log.d("SampleService.prepareForeground","called")
+        // https://developer.android.com/guide/components/foreground-services?hl=ja
+        // 通知をタップしたときにこのアクテビティを開く
+        val pendingIntent: PendingIntent =
+            Intent(applicationContext, MainActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(applicationContext, 0, notificationIntent, 0)
+            }
+
+        // 通知の作成
+        // https://qiita.com/naoi/items/03e76d10948fe0d45597#minsdkversion%E3%81%8C26%E4%BB%A5%E4%B8%8A%E3%81%AE%E5%A0%B4%E5%90%88
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // 通知チャンネルの作成
+        // https://developer.android.com/guide/topics/ui/notifiers/notifications?hl=ja#ManageChannels
+        val name = getText(R.string.notification_title)
+        val channelId = getString(R.string.app_name)
+        val notifyDescription = getString(R.string.notification_message)
+
+        if (manager.getNotificationChannel(channelId) == null) {
+            val mChannel = NotificationChannel(
+                channelId,
+                name,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            mChannel.apply {
+                description = notifyDescription
+            }
+            manager.createNotificationChannel(mChannel)
+        }
+
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle(channelId)
+            .setContentText(notifyDescription)
+            .setSmallIcon(R.drawable.ic_android_black_24dp)
+            .setContentIntent(pendingIntent)
+            // 5.0 以降 ティッカーは設定しても表示されない
+            // https://phicdy.hatenablog.com/entry/android-notification-differences
+            //.setTicker(getText(R.string.ticker_text))
+            .build()
+
+        // Notification ID cannot be 0.
+        startForeground(ONGOING_NOTIFICATION_ID, notification)
+    }
+}
